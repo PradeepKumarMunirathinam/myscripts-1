833f8da9ec70b92dfc82b5e15871d4335f5d9b06
PreetiCK <PreetiCK@users.noreply.github.com>
2015-08-21 12:42:09 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -5,5 +5,5 @@ apt-get update
 wget -qO- https://get.docker.com/ | sh
 
 
-docker run --name nginx_devops -d -p 80:80 -e NGINX_ADDR=orchardnginx.southeastasia.cloudapp.azure.com -e JENKINS_ADDR=orchardjenkins.southeastasia.cloudapp.azure.com -e SONAR_ADDR=orchardsonar.southeastasia.cloudapp.azure.com preetick/nginx-devops:v13
+docker run --name nginx_devops -d -p 80:80 -e NGINX_ADDR=orchard.southeastasia.cloudapp.azure.com -e JENKINS_ADDR=orchardjenkins.southeastasia.cloudapp.azure.com -e SONAR_ADDR=orchardsonar.southeastasia.cloudapp.azure.com preetick/nginx-devops:v13
 

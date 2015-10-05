3001e13db76a0d253d879a412c16ac5f47473acb
PreetiCK <PreetiCK@users.noreply.github.com>
2015-10-05 18:30:40 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,4 +4,4 @@ apt-get update
 wget -qO- https://get.docker.com/ | sh
 
 chmod 777 /home/orchard
-docker run --name myjenkins -d -p 8080:8080 -p 50000:50000 -v /home/orchard:/var/jenkins_home preetick/jenkins-devops:v5
+docker run --name myjenkins -d -p 8080:8080 -p 50000:50000 -v /home/orchard/jen:/var/jenkins_home preetick/jenkins-devops:v5

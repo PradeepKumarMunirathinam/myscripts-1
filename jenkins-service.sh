1cbd0d56e13891585b3df2a0aa8b5729101509af
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-04-01 21:35:51 +0530

    Update jenkins-service.sh

--- jenkins-service.sh
+++ jenkins-service.sh
@@ -1,4 +1,5 @@
-wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get -y install jenkins
+sudo su
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get -y install jenkins

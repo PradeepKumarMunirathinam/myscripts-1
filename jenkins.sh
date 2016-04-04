1c25f89542ea8ea4be6c2f91f1e644d3323987cf
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-04-04 15:43:39 +0530

    Update jenkins.sh
    modified init 6 as a comment

--- jenkins.sh
+++ jenkins.sh
@@ -18,4 +18,4 @@ sudo wget -O /etc/init.d/jenkins-startup https://raw.githubusercontent.com/ariji
 sudo chmod ugo+x /etc/init.d/jenkins-startup 
 sudo update-rc.d jenkins-startup defaults
 
-sudo init 6
+#sudo init 6

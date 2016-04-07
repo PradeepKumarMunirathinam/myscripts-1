7c51de0f66bcc91b09f54e6faacb356a667a7637
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-04-07 17:14:45 +0530

    Update docker-test.sh

--- docker-test.sh
+++ docker-test.sh
@@ -2,4 +2,4 @@ wget -qO- https://get.docker.com/ | sh
 
 sudo docker pull devopsmt/jenkins-dockerindocker:v1
 
-docker run --privileged=true -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock devopsmt/jenkins-dockerindocker:v1 
+sudo docker run --privileged=true -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock devopsmt/jenkins-dockerindocker:v1 

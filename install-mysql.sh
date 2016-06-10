0bf11bf345b9661ec8ac4467253ab0a927045755
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-06-10 13:33:35 +0530

    Update install-mysql.sh

--- install-mysql.sh
+++ install-mysql.sh
@@ -17,3 +17,5 @@ sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my
 sudo service mysql restart
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
 echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
+wget https://adidaswarstorage.blob.core.windows.net/sqlfileupload/myarenadb_script.sql 
+mysql -u root -pwelcome123 < myarenascript.sql

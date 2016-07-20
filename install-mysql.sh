43af4a461c2f434a5326dfb2b71d006e66b6e589
PradeepKumarMunirathinam <Pradeep.Munirathinam@mindtree.com>
2016-07-20 16:44:04 +0530

    Update install-mysql.sh

--- install-mysql.sh
+++ install-mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-apt-get update
+sudo apt-get update
 
 # set up a silent install of MySQL
 
 sudo mkdir -p /opt/dbscript
 #cd /opt/dbscript && wget https://adidaswarstorage.blob.core.windows.net/sqlfileupload/myarenadb_script.sql
 export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6.13 mysql-server/root_password password welcome@123 | debconf-set-selections
-echo mysql-server-5.6.13 mysql-server/root_password_again password welcome@123 | debconf-set-selections
+sudo mysql-server-5.6.13 mysql-server/root_password password welcome@123 | debconf-set-selections
+sudo mysql-server-5.6.13 mysql-server/root_password_again password welcome@123 | debconf-set-selections
 
 # install the LAMP stack
 apt-get -y install mysql-server 
@@ -16,5 +16,5 @@ sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my
 
 sudo service mysql restart
 #mysql -u root -pwelcome123 < /opt/dbscript/myarenadb_script.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome@123'; flush privileges;" | mysql -u root -pwelcome@123
+sudo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome@123'; flush privileges;" | mysql -u root -pwelcome@123
 #echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123

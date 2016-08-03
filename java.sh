deb3f0c434f7eb2e759b8384cc10236c15539118
PradeepKumarMunirathinam <Pradeep.Munirathinam@mindtree.com>
2016-08-03 17:32:46 +0530

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,7 +1,8 @@
 #!/bin/sh  
 if [ -d /opt/java/ ]
 then
-:
+rm -rf /opt/java
+mkdir /opt/java
 else
 mkdir /opt/java 
 fi 
@@ -18,3 +19,25 @@ update-alternatives --config jar
 export JAVA_HOME=/opt/java/jdk1.8.0_45/
 export JRE_HOME=/opt/java/jdk1.8.0._45/jre
 export PATH=$PATH:/opt/java/jdk1.8.0_45/bin:/opt/java/jdk1.8.0_45/jre/bin
+
+#Tomcat installation
+if [ -d /opt/tomcat ]
+then
+rm -rf /opt/tomcat
+mkdir /opt/tomcat
+else
+mkdir /opt/tomcat
+fi
+cd /opt/tomcat/
+wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.zip
+wget https://www.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.zip.md5
+cat /opt/tomcat/apache-tomcat-8.0.36.zip.md5
+md5sum /opt/tomcat/apache-tomcat-8.0.36.zip
+unzip apache-tomcat-8.0.36.zip
+cd /opt/tomcat/apache-tomcat-8.0.36/bin/
+chmod 700 *.sh
+cd /opt/tomcat/apache-tomcat-8.0.36/webapps
+wget https://adidaswarstorage.blob.core.windows.net/warfileupload/MyArenaREST.war
+cd /opt/tomcat/apache-tomcat-8.0.36/bin/ 
+sudo sh /opt/tomcat/apache-tomcat-8.0.36/bin/startup.sh
+

64e616aeaaa146631b438d99b5f7c7c43d768611
PradeepKumarMunirathinam <Pradeep.Munirathinam@mindtree.com>
2016-08-26 16:51:34 +0530

    Create myarenaexpectscripts.sh

---
+++ myarenaexpectscripts.sh
@@ -0,0 +1,6 @@
+cd /home/lx_jeralron
+wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/calling.sh
+wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/copycmd.sh
+wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/copycmd2.sh
+wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/copywar.exp
+wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/copywar2.exp

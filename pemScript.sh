dcd6e1e2bd13e9d8423994993ba89df77b8a7c72
PradeepKumarMunirathinam <Pradeep.Munirathinam@mindtree.com>
2016-08-30 14:35:10 +0530

    Update pemScript.sh

--- pemScript.sh
+++ pemScript.sh
@@ -40,11 +40,11 @@ jGVeADfQeS6TH/V9wZGNbDJjsqPGbq/rsauBQ4krObPzULCQUksqfphaaINkcUTA
 
 cd /home/lx_jeralron
 
-wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/calling.sh
-wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/copycmd.sh
-wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/copycmd2.sh
-wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/copywar.exp
-wget https://myarenaprod4098.blob.core.windows.net/scripts/scripts/copywar2.exp
+wget https://myarenaprod4098.blob.core.windows.net/scripts/calling.sh
+wget https://myarenaprod4098.blob.core.windows.net/scripts/copycmd.sh
+wget https://myarenaprod4098.blob.core.windows.net/scripts/copycmd2.sh
+wget https://myarenaprod4098.blob.core.windows.net/scripts/copywar.exp
+wget https://myarenaprod4098.blob.core.windows.net/scripts/copywar2.exp
 
 
 chmod 755 calling* copy*

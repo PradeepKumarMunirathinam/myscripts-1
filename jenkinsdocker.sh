d41686f1a6636a44d0521e5e1c553a088897f695
PradeepKumarMunirathinam <Pradeep.Munirathinam@mindtree.com>
2016-11-15 13:14:28 +0000

    Update jenkinsdocker.sh

--- jenkinsdocker.sh
+++ jenkinsdocker.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sudo apt-get update -y
 wget -qO- https://get.docker.com/ | sh
-sudo docker run --name jenkins  -p 8080:8080  jenkins --prefix=/jenkins
+#sudo docker run --name jenkins  -p 8080:8080  jenkins --prefix=/jenkins
+sudo docker run -d --name jenkins  -p 8080:8080  jenkins --prefix=/jenkins
+#sudo docker run -d -p 8080:8080 
